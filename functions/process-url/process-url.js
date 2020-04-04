b908d6a80dc7bb6be91973cf730a5683dbb65c10
Joel Hooks <joelhooks@gmail.com>
2020-04-04 16:18:38 -0700

    hi 👋

---
+++ functions/process-url/process-url.js
@@ -0,0 +1,41 @@
+const cloudinary = require("cloudinary").v2;
+const qs = require("querystring");
+cloudinary.config({
+  cloud_name: "sector",
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+
+exports.handler = async function(event, ctx) {
+  const { queryStringParameters } = event;
+  console.log(queryStringParameters);
+  try {
+    // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
+    const imageUrl = cloudinary.url(
+      `${process.env.CHRIS_IMAGE_VERSION}/og-images/img-2.png`,
+      {
+        // resouce_type: "raw"
+        sign_url: true,
+        // secure: true,
+        custom_pre_function: {
+          function_type: "remote",
+          source: `https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+            queryStringParameters
+          )}`
+        }
+      }
+    );
+    console.log(`https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+            queryStringParameters
+          )}`)
+    return {
+      statusCode: 302,
+      headers: {
+        Location: imageUrl
+      },
+      body: ""
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
