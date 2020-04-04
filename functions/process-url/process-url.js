ca063f9c7dd5af603c0e7e4d7cfbe24c0efa962b
Joel Hooks <joelhooks@gmail.com>
2020-04-04 16:22:07 -0700

    update

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,14 +1,14 @@
-const cloudinary = require("cloudinary").v2;
-const qs = require("querystring");
+const cloudinary = require('cloudinary').v2
+const qs = require('querystring')
 cloudinary.config({
-  cloud_name: "sector",
+  cloud_name: 'joelhooks',
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
-});
+  api_secret: process.env.CLOUDINARY_SECRET,
+})
 
-exports.handler = async function(event, ctx) {
-  const { queryStringParameters } = event;
-  console.log(queryStringParameters);
+exports.handler = async function (event, ctx) {
+  const {queryStringParameters} = event
+  console.log(queryStringParameters)
   try {
     // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
     const imageUrl = cloudinary.url(
@@ -18,24 +18,26 @@ exports.handler = async function(event, ctx) {
         sign_url: true,
         // secure: true,
         custom_pre_function: {
-          function_type: "remote",
-          source: `https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`
-        }
-      }
-    );
-    console.log(`https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`)
+          function_type: 'remote',
+          source: `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+            queryStringParameters,
+          )}`,
+        },
+      },
+    )
+    console.log(
+      `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+        queryStringParameters,
+      )}`,
+    )
     return {
       statusCode: 302,
       headers: {
-        Location: imageUrl
+        Location: imageUrl,
       },
-      body: ""
-    };
+      body: '',
+    }
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-};
+}
